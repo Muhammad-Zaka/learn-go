0093b875c5afb99a4b7bc183198f0ccd6857cace
muhammad-zaka <m.zkaurrehman@gmail.com>
2021-11-29 23:33:41 +0500

    P1 Complete

--- main.go
+++ main.go
@@ -8,10 +8,9 @@ import (
 
 func main() {
 
-	PI := 3.14
+	const PI = 3.14
 	radius := 5
 	circumference := 2 * PI * float64(radius)
-
 	fmt.Printf("For a radius of %v. The circumference is %v", radius, circumference)
 	fmt.Println(handlers.Handle)
 }
